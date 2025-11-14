d6ff2df3e01c6610470e94283e29d0059f4b516b
Christina Papadodimitraki <christina-v7@hotmail.com>
2025-11-14 22:32:20 +0100

    AudioSFXMenu

--- Assets/Scripts/Managers/Main Menu.cs
+++ Assets/Scripts/Managers/Main Menu.cs
@@ -1,9 +1,13 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] GameObject stats_panel;
+
+    Dictionary<int, Action> menu_action;
 
     private void Awake()
     {
@@ -12,30 +16,37 @@ public class MainMenu : MonoBehaviour
 
     private void Start()
     {
+        menu_action = new Dictionary<int, Action>();
+        menu_action.Add(0, Start_Game);
+        menu_action.Add(1, Open_Settings_Menu);
+        menu_action.Add(2, Open_Stats_Menu);
+        menu_action.Add(3, Quit_Game);
         AudioManager.Instance.PlayMusic(AudioManager.Instance.mainMenuMusic);
+    }
+
+    public void Menu_Action(int id)
+    {
+        AudioManager.Instance.PlaySFX(AudioManager.Instance.buttonEffecct);
+        menu_action[id]();
     }
 
     public void Start_Game()
     {
-        AudioManager.Instance.PlaySFX(AudioManager.Instance.buttonEffecct);
         SceneManager.LoadSceneAsync("VampireSurvivors");
     }
 
     public void Open_Settings_Menu()
     {
-        AudioManager.Instance.PlaySFX(AudioManager.Instance.buttonEffecct);
         SettingsMenu.sm_instance.gameObject.SetActive(true);
     }
 
     public void Open_Stats_Menu()
     {
-        AudioManager.Instance.PlaySFX(AudioManager.Instance.buttonEffecct);
         stats_panel.SetActive(true);
     }
 
     public void Quit_Game()
     {
-        AudioManager.Instance.PlaySFX(AudioManager.Instance.buttonEffecct);
         Application.Quit();
     }
 }
